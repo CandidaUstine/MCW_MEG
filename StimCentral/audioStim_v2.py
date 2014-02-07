e7aa490cbfea9adb0e78ec5fa5e45d8f8d27f96d
CandidaUstine <cu38@cornell.edu>
2014-02-07 11:12:26 -0600

    auditory odd ball in psychopy

--- StimCentral/audioStim_v2.py
+++ StimCentral/audioStim_v2.py
@@ -6,7 +6,7 @@ If you publish work using this script please cite the relevant PsychoPy publicat
   Peirce, JW (2007) PsychoPy - Psychophysics software in Python. Journal of Neuroscience Methods, 162(1-2), 8-13.
   Peirce, JW (2009) Generating stimuli for neuroscience using PsychoPy. Frontiers in Neuroinformatics, 2:10. doi: 10.3389/neuro.11.010.2008
 
-Experiment coded by Candida Jane Maria Ustine  -  custine@mcw.edu
+Experiment coded by Candida Jane Maria Ustine - custine@mcw.edu
 Created on January 23rd, 2014
 Updates: 
 
