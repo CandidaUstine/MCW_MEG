ac932b759a2752f6a2c12523041aa9591a8e0b51
CandidaUstine <cu38@cornell.edu>
2014-02-10 15:50:53 -0600

    Updated subj dir, EOG channgel Type (202) and channel number

--- ssp_find_eog_event.m
+++ ssp_find_eog_event.m
@@ -1,4 +1,4 @@
-function ssp_find_eog_event_cu(subjID,exp, runnum)
+function ssp_find_eog_event(subjID, exprum)
 
 %input file
 %in_fif_File = infif;
@@ -9,14 +9,11 @@ function ssp_find_eog_event_cu(subjID,exp, runnum)
 % eog_eventFileName = [inpath, name1,'_', name2, '_eog-eve.fif'];
 
 
-inpath = '/autofs/cluster/kuperberg/SemPrMM/MEG';
+inpath = '/home/custine/MEG/data/msabri/';
 
-in_fif_File = [inpath '/data/' subjID '/' subjID '_' exp,runnum,'_raw.fif'];
+in_fif_File = [inpath subjID '/' subjID '_' exprum,'_raw.fif'];
 %in_fif_File = [inpath '/data/' subjID '/' subjID '_ATLLoc_raw.fif'];
-eog_eventFileName = [inpath '/data/' subjID '/ssp/' subjID '_' exp,runnum,'_eog-eve.fif'];
-%eog_eventFileName = [inpath '/data/' subjID '/ssp/' subjID '_ATLLoc_eog-eve.fif'];
-%eog_eventFileName='/autofs/cluster/kuperberg/SemPrMM/MEG/data/ac23/ssp/ac23_BaleenHPRun1_eog-eve.fif';
-%eog_figfile='/autofs/cluster/kuperberg/SemPrMM/MEG/data/sc1/ssp/sc1_MaskedMMRun22BaleenLP_m2sd_eog.png';
+eog_eventFileName = [inpath subjID '/ssp/' subjID '_' exprum,'_eog-eve.fif'];
 
 %reading eog channels from data files
 [fiffsetup] = fiff_setup_read_raw(in_fif_File);
@@ -25,33 +22,26 @@ ch_EOG = strmatch('EOG',channelNames);
 sampRate = fiffsetup.info.sfreq;
 start_samp = fiffsetup.first_samp;
 end_samp = fiffsetup.last_samp;
-[eog] = fiff_read_raw_segment(fiffsetup, start_samp ,end_samp, ch_EOG(2));
+[eog] = fiff_read_raw_segment(fiffsetup, start_samp ,end_samp, ch_EOG(1));
 
 % Detecting Blinks
-filteog = ssp_eegfilt(eog, sampRate,0,10);
-EOG_type = 998;
+filteog = eegfilt(eog, sampRate,0,10);
+EOG_type = 202;
 firstSamp = fiffsetup.first_samp;
 temp = filteog-mean(filteog);
  
-eog_std_dev_value=.5; %Change according to the subject(Default 1) (Higher number- detect only distict narrow peaks) 
+eog_std_dev_value=1; %Change according to the subject(Default 1) (Higher number- detect only distict narrow peaks) 
 
 if sum(temp>(mean(temp)+1*std(temp))) > sum(temp<(mean(temp)+1*std(temp)))
     
-    eog_events = ssp_peakfinder((filteog),eog_std_dev_value*std(filteog),-1);
+    eog_events = peakfinder((filteog),eog_std_dev_value*std(filteog),-1);
 
 else
-    eog_events = ssp_peakfinder((filteog),eog_std_dev_value*std(filteog),1);
+    eog_events = peakfinder((filteog),eog_std_dev_value*std(filteog),1);
 
 end
 
-%  t=1:length(filteog);
-%  plot(t,filteog);
-%  hold on
-%  grid on
-%  axis tight
-%  plot(t(eog_events),filteog(eog_events),'r+')
-%  print( gcf, '-dpng', eog_figfile )
 
-ssp_writeEventFile(eog_eventFileName, firstSamp, eog_events, EOG_type);
+writeEventFile(eog_eventFileName, firstSamp, eog_events, EOG_type);
 
 end
