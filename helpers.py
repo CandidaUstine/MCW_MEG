b1631d4a65ab2bd2a076cc5e23b65e775741a4f1
CandidaUstine <cu38@cornell.edu>
2014-02-11 15:52:05 -0600

    ssp event creation script updated

--- helpers.py
+++ helpers.py
@@ -8,14 +8,17 @@ def ssp_event_creator(subjID, event):
 
  fiffs = []
  in_path = pj('/home/custine/MEG/data/msabri/', subjID)  ### TO CHANGE HERE
- fiffs = glob(pj(in_path,'*_raw.fif'))
+ fiffs = glob(pj('*_raw.fif'))
  fiffs[:] = filter(lambda x: 'EmptyRoom' not in x and 'Spont' not in x, fiffs) ### TO Change here 
  fiffs.sort()
+ cmd = ['warning off all']
+ line = "addpath('/home/custine/MEG/scripts/');"
+ cmd.append(line)
  if event == 'ecg': 
    mlab = "ssp_find_ecg_event( '%s','%s');"
  elif event == 'eog':
    mlab = "ssp_find_eog_event('%s', '%s');"
- cmd = ['warning off all']
+
  for fif in fiffs:
      cmd.append(mlab % (subjID, fif))
  cmd.append('exit;')
