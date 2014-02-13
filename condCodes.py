9227202316bb00932de1175116571a4473c469c7
CandidaUstine <cu38@cornell.edu>
2014-02-13 12:17:10 -0600

    First commit: conditions and codes for msabri dual auditory exp

---
+++ condCodes.py
@@ -0,0 +1,41 @@
+codes = {'Left':('3', '4', '100', '200', '400', '500'),
+         'Right':('3', '4', '100', '200', '400', '500'),
+         'LeftDual':('3', '4', '100', '200', '400', '500'),
+         'RightDual':('3', '4', '100', '200', '400', '500'),
+
+
+condLabels = {'Left':
+                    [['3', 'dev_detected'],
+                    ['4', 'dev_undetected'],
+                    ['100', 'standard_corr'],
+                    ['200', 'standard_incorr'],
+                    ['400', 'standard_dev_seq_corr'],
+                    ['500', 'standard_dev_seq_incorr']],
+              'Right':
+                    [['3', 'dev_detected'],
+                    ['4', 'dev_undetected'],
+                    ['100', 'standard_corr'],
+                    ['200', 'standard_incorr'],
+                    ['400', 'standard_dev_seq_corr'],
+                    ['500', 'standard_dev_seq_incorr']],
+              'LeftDual':
+                    [['3', 'dev_detected'],
+                    ['4', 'dev_undetected'],
+                    ['100', 'standard_corr'],
+                    ['200', 'standard_incorr'],
+                    ['400', 'standard_dev_seq_corr'],
+                    ['500', 'standard_dev_seq_incorr']],
+              'RightDual':
+                    [['3', 'dev_detected'],
+                    ['4', 'dev_undetected'],
+                    ['100', 'standard_corr'],
+                    ['200', 'standard_incorr'],
+                    ['400', 'standard_dev_seq_corr'],
+                    ['500', 'standard_dev_seq_incorr']]
+                    }
+            
+epMax = {'Left': '.4',
+         'Right': '.4',
+         'LeftDual': '.4',
+         'RightDual': '.4'
+        }
