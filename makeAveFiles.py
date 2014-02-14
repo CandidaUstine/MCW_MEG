73b5b1bdf911327b9c575201980c1463f6ce7107
CandidaUstine <cu38@cornell.edu>
2014-02-14 11:53:28 -0600

    makeAveFiles.py: minor edits

--- makeAveFiles.py
+++ makeAveFiles.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 13 12:59:34 2014
+
+@author: custine
+"""
+
+
 ###makeAveFiles.py script to create the ave files that are used when creating the ave.fif average files in mne using mne_process_raw
 
 
@@ -30,7 +38,7 @@ def makeAveFiles(subjID, projType):
         myFile.write('average{\n')
         myFile.write('\tname\t\"'+exp+' Averages\"\n')
         myFile.write('\toutfile\t\t'+subjID+'_'+exp+'-ave.fif\n')
-        myFile.write('\tlogfile\t\t./logs/'+subjID+'_'+exp+'-ave.log\n')
+        myFile.write('\tlogfile\t\t'+filePrefix+'/logs/'+subjID+'_'+exp+'-ave.log\n')
         
         if projType == 'projon':
             myFile.write('\teventfile\t'+filePrefix+'/eve/'+subjID+'_mod.eve\n\n')
@@ -38,8 +46,8 @@ def makeAveFiles(subjID, projType):
             myFile.write('\tmagReject\t'+magRej+'\n')
             myFile.write('\teegReject\t'+eegRej+'\n')
             myFile.write('\tgradFlat\t'+gradFlat+'\n')
-            myFile.write('\tmagFlat\t'+magFlat+'\n')            
-            myFile.write('\teegFlat\t'+eegFlat+'\n\n')
+            myFile.write('\tmagFlat\t\t'+magFlat+'\n')            
+            myFile.write('\teegFlat\t\t'+eegFlat+'\n\n')
             
         for item in condDict[exp]:
             myFile.write('\tcategory {\n')
