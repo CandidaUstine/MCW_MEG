d13eb7d61cf06c558964071a9a5e423c3ff83ebe
CandidaUstine <cu38@cornell.edu>
2014-02-14 11:52:42 -0600

    preProc_avg.py: Updated with writing evoked structure... final version

--- preProc_avg.py
+++ preProc_avg.py
@@ -3,6 +3,8 @@
 Created on Thu Feb 13 16:59:34 2014
 
 @author: custine
+Usage: python preProc_avg.py subjID expName
+Example: python preProc_avg.py ac1 Left 
 """
 
 import mne
@@ -33,7 +35,7 @@ projVal = True
 avgRefVal = False
 
 ###Filtering
-hp_cutoff = 0.5
+hp_cutoff = 0.7
 lp_cutoff = 20
 
 #######Experiment specific parameters 
@@ -62,9 +64,44 @@ gradFlat = 1000e-15
 
 evoked=[]
 #print ev 
+
+##Setup Subject Speciifc Information
 data_path = '/home/custine/MEG/data/msabri/' +subjID
 event_file = data_path + '/eve/' + subjID + '_'+ exp + evSuffix
 print event_file
+
+raw_file = data_path +'/'+ subjID +'_' +exp + '_raw.fif' ##Change this suffix if you are using SSP 
+avgLog_file = data_path + '/ave_projon/logs/' +subjID + '_' + exp + '_py-ave.log'
+
+
+##Setup Reading fiff data structure
+print 'Reading Raw data... '
+raw = fiff.Raw(raw_file, preload = True)
+events = mne.read_events(event_file)
+
+raw_skip = raw.first_samp
+mne.set_log_file(fname = avgLog_file, overwrite = True)
+
+##Filter raw data 
+fiff.Raw.filter(raw, l_freq = hp_cutoff, h_freq = lp_cutoff)
+
+##Pick all channels 
+picks = []
+for i in range(raw.info['nchan']):
+    picks.append(i)
+    
+##Read Epochs and compute Evoked :) 
+print 'Reading Epochs from raw file...'
+epochs = mne.Epochs(raw, events, event_id, tmin, tmax, baseline = (None,0), picks = picks, proj = True, name = None, preload = True, flat = dict(mag = magFlat, grad= gradFlat), reject=dict(mag=magRej, grad=gradRej))
+print epochs
+evoked = [epochs[cond].average(picks =picks) for cond in event_id]
+
+##Write Evoked 
+print 'Writing Evoked data to -ave.fif file...' 
+fiff.write_evoked(data_path + '/ave_projon/'+subjID+'_' +exp+'-ave.fif', evoked)
+
+print 'Completed! See ave.fif result in folder', data_path + '/ave_projon/'
+
 
 
 
