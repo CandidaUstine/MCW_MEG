df8d5ca8b4330116950cf8321efa761202b2ae38
CandidaUstine <cu38@cornell.edu>
2014-02-21 16:24:03 -0600

    Modified to run for any experimenter/paradigm

--- preProc_avg.sh
+++ preProc_avg.sh
@@ -1,57 +1,42 @@
 #!/bin/csh -f
 
-#usage: preProc_avg subjID
+#usage: preProc_avg exp subjID par log-optional
 
 if ( $#argv == 0 ) then 
     echo "Hey dummy! Enter a subject! :)"
     exit 1
 endif
 
-if ( $#argv == 1 ) then
-    set log='./preProc_avg.log'
-    echo "Logging to default log..." >>& $log
+#if log exists, delete
+if ( -e /home/custine/MEG/data/{$1}/{$2}/ave_projon/logs/preProc_avg.log ) then
+    rm /home/custine/MEG/data/{$1}/{$2}/ave_projon/logs/preProc_avg.log
 endif
 
-if ( $#argv == 2) then
-    set log=$2
+if ( $#argv == 3 ) then
+    #set log = '/home/custine/MEG/data/{$1}/{$2}/ave_projon/logs/preProc_avg.log'
+    touch /home/custine/MEG/data/{$1}/{$2}/ave_projon/logs/preProc_avg.log
+    echo "Logging to default log..." >>& /home/custine/MEG/data/{$1}/{$2}/ave_projon/logs/preProc_avg.log
 endif
 
-# if log exists, delete
-if ( -e $log ) then
-    rm $log
+if ( $#argv == 4) then
+    set log=$4
 endif
 
+echo $3
 foreach proj ('projon') ##Enter projoff here if needed! :) 
-        echo "Making Average Parameter files -.ave" >>& $log 
-        python /home/custine/MEG/scripts/makeAveFiles.py $1 $proj >>& $log 
+        echo $3
+
+        echo "Making Average Parameter files -.ave" >>& /home/custine/MEG/data/{$1}/{$2}/ave_projon/logs/preProc_avg.log
+        python /home/custine/MEG/scripts/makeAveFiles.py $2 $proj >>& /home/custine/MEG/data/{$1}/{$2}/ave_projon/logs/preProc_avg.log 
   
-        echo "Making average fif files..." >>& $log 
-        cd /home/custine/MEG/data/msabri/$1/ave_$proj
+        echo "Making average fif files..." >>& /home/custine/MEG/data/{$1}/{$2}/ave_projon/logs/preProc_avg.log 
+        cd /home/custine/MEG/data/$1/$2/ave_$proj
 
-        ##Left average 
+        ##compute mne average 
         mne_process_raw \
-        --raw ../$1_Left_raw.fif \
-        --ave ../ave/$1_Left.ave \
-        --$proj --lowpass 20 --highpass 0.5 --filtersize 8192>>& $log
-
-        ##Right average 
-        mne_process_raw \
-        --raw ../$1_Right_raw.fif \
-        --ave ../ave/$1_Right.ave \
-        --$proj --lowpass 20 --highpass 0.5 --filtersize 8192>>& $log
-
-        ##LeftDual average 
-        mne_process_raw \
-        --raw ../$1_LeftDual_raw.fif \
-        --ave ../ave/$1_LeftDual.ave \
-        --$proj --lowpass 20 --highpass 0.5 --filtersize 8192>>& $log
-
-        ##RightDual average 
-        mne_process_raw \
-        --raw ../$1_RightDual_raw.fif \
-        --ave ../ave/$1_RightDual.ave \
-        --$proj --lowpass 20 --highpass 0.5 --filtersize 8192>>& $log
-
+        --raw ../$2_$3_raw.fif \
+        --ave ../ave/$2_$3.ave \
+        --$proj --lowpass 20 --highpass 0.7 --filtersize 8192 >>& /home/custine/MEG/data/{$1}/{$2}/ave_projon/logs/preProc_avg.log
 end
 
 
