b1d0429542b47f755e0d7d6688e098e218da1b24
CandidaUstine <cu38@cornell.edu>
2014-02-25 14:16:48 -0600

    version2: Using the channel numbers(using py version of chan numbers) and plotting a single figure with MEG sensors arranged according to topo groups

--- sensor_plot_evokedColor.py
+++ sensor_plot_evokedColor.py
@@ -35,78 +35,31 @@ results_path = data_path +'plots/'
 fname = data_path + subjID +'_' +par+'-ave.fif'
 out_fname = results_path + subjID + '_' + par +'_'+set1+'_meg_evoked_color.png'
 print out_fname
-chan_path = '/home/custine/MEG/scripts/function_inputs/MEG_Chan_Names/'
-#chan_names = readInput.readList(chan_fname)
-chan_list = []
+#chan_path = '/home/custine/MEG/scripts/function_inputs/MEG_Chan_Names/'
+chan_fname = '/home/custine/MEG/scripts/function_inputs/MEG_Chan_Number/MEG_chan_number_list-m.txt'
+chan_names = readInput.readList(chan_fname)
+#chan_list = []
 #for item in chan_names:
 #    chan_list.append('MEG'+item)
-#grp_list = ['Vertex', 'Left-frontal','Left-temporal', 'Left-parietal', 'Left-occipital', 'Right-frontal', 'Right-temporal', 'Right-parietal', 'Right-occipital']
+chan_names = [int(chan) for chan in chan_names]
 
-chan_groups = ['Frontal', 'Parietal', 'Temporal', 'Occipital']
-hem = ['L', 'R']
-data = []
-for grp in chan_groups:
-    font = {'size': 8}
-    plt.rc('font', **font)
-    plt.subplots_adjust(hspace=0.2, wspace =0.2) 
-    #pl.axes(frameon=False)
-    for h in hem : 
-        chan_list =[]
-        chan_fname = chan_path + 'chan_' + h + grp +'.txt'
-        chan_names = readInput.readList(chan_fname)
-        for item in chan_names:
-            chan_list.append('MEG'+item)
-
-        #print chan_list
-        if h == 'L':
-            if grp == 'Frontal':
-                plt.subplot(3,4,2)
-                #plt.title('Left Frontal', fontsize = 'large')
-            elif grp == 'Temporal':
-                plt.subplot(3,4,5)
-                #plt.title('Left Temporal', fontsize = 'large')
-            elif grp == 'Parietal':
-                plt.subplot(3,4,6)
-                #pl.title()
-                #plt.title('Left Parietal', fontsize = 'large')
-            elif grp == 'Occipital':
-                plt.subplot(3,4,10)
-                #plt.title('Left Occipital', fontsize = 'large')
-               # pl.text(10,20,'Left Frontal')
-                
-        else:
-            if grp == 'Frontal':
-                plt.subplot(3,4,3)
-                #plt.title('Right Frontal', fontsize = 'large')
-            elif grp == 'Temporal':
-                plt.subplot(3,4,8)
-                #plt.title('Right Temporal', fontsize = 'large')
-            elif grp == 'Parietal':
-                plt.subplot(3,4,7)
-                #plt.title('Right Parietal', fontsize = 'large')
-            elif grp == 'Occipital':
-                plt.subplot(3,4,11)            
-                #plt.title('Right Occipital', fontsize = 'large')  
-
-        ####Reading the Evoked data structure
-        for c in condName:
-            print c
-            evoked = mne.fiff.Evoked(fname, setno='epochs_'+c , baseline = (None, 0))
-            #evoked = mne.fiff.Evoked(fname, setno = c , baseline = (None, 0)) ##Use this if you are using condition numbers
-            badChanSet = set(evoked.info['bads'])
-            print c
-            sel = mne.fiff.pick_channels(evoked.ch_names,include=chan_list)
-            #sel = mne.fiff.pick_types(evoked.info,meg=True, eeg=False, exclude = 'bads')
-            #sel = mne.read_selection(name = grp_list)
-            print sel
-            data.append(evoked.data[sel])
-            #evoked.data
-            print data
-            ##Plotting the epochs for all the MEG sensors across time
-            #plt.ylabel('MEG Sensors')
-            #plt.xlabel('Time in msec')
-            #plt.title('Colour map of MEG sensors evoked data')
-            plt.imshow(data, cmap=get_cmap("Jet"), interpolation=None)
-plt.colorbar()
-plt.show()
-plt.savefig(out_fname)
+####Reading the Evoked data structure
+for c in condName:
+    print c
+    evoked = mne.fiff.Evoked(fname, setno='epochs_'+c , baseline = (None, 0))
+    #evoked = mne.fiff.Evoked(fname, setno = c , baseline = (None, 0)) ##Use this if you are using condition numbers
+    #badChanSet = set(evoked.info['bads'])
+    sel = chan_names
+    #sel = list(set(chan_names) - set(badChanSet))
+    print sel
+    data = evoked.data[sel]
+    print data
+    ##Plotting the epochs for all the MEG sensors across time
+    plt.ylabel('MEG Sensors')
+    plt.xlabel('Time in msec')
+    plt.title('Colour map of MEG sensors evoked data')
+    #plt.tick_params()
+    plt.imshow(data, cmap=get_cmap("hsv"), interpolation=None)
+    plt.colorbar()
+    #plt.show()
+    plt.savefig(out_fname)
