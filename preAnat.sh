172c0d18ce1bb7c11eea9d2e4749b80849f7d1ab
CandidaUstine <cu38@cornell.edu>
2014-02-28 16:49:22 -0600

    preAnat.sh - minor updates

--- preAnat.sh
+++ preAnat.sh
@@ -5,30 +5,40 @@
 #Usage: ./preAnat.sh exp subjID
 
 
+setenv SUBJECT $2
+set BEM_METHOD = WATER 
+
+remove teh existing log file
+if ( -e /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log ) then
+    rm /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log
+endif
+
 if ( $#argv == 3 ) then
     #set log = '/home/custine/MEG/data/{$1}/{$2}/ave_projon/logs/preProc_avg.log'
     touch /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log
     echo "Logging to default log..." >>& /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log
 endif
 
-setenv SUBJECT $1 
-set BEM_METHOD = WATER 
-
 echo "Hello! Make sure the Cortical surface reconstructions have been created for your subject before you run this step..." 
 
 #Setting up the anatomical MR images for MRILab 
 echo "Setting up the anatomical MR images for MRILab..."
-mne_setup_mri >>& /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log 
+mne_setup_mri --overwrite >>& /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log 
 
 #Setting up the Source Space(creating a decimated dipole grid on the wm surface - default 7mm grid spacing)
 echo "Setting up the Source Space..."
 mne_setup_source_space >>& /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log 
 
 #Creating the BEM Model meshes 
-echo"Creating the BEM meshes..."
+echo "Creating the BEM meshes..."
 echo "Using the Watershed method to create the surfaces"
 mne_watershed_bem --overwrite >>& /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log
-
+cd /home/custine/MRI/structurals/subjects/$2/bem
+rm *.surf
+ln -s ./watershed/$2_outer_skin_surface outer_skin.surf
+ln -s ./watershed/$2_outer_skull_surface outer_skull.surf
+ln -s ./watershed/$2_inner_skull_surface inner_skull.surf
+#mv $SUBJECTS_DIR/$1/bem/$1-bem.fif $SUBJECTS_DIR/$1/bem/$1-orig-bem.fif
 
 #Setting up the BEM 
 mne_setup_forward_model --surf --ico 4  >>& /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log
