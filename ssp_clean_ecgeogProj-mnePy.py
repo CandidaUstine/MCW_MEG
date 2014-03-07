8e4479d55c1b1589c7d9def0c6a691306fde84de
CandidaUstine <cu38@cornell.edu>
2014-03-07 15:03:11 -0600

    Now working - created a new ssp_raw.fif file depending on type and number of proj specified

--- ssp_clean_ecgeogProj-mnePy.py
+++ ssp_clean_ecgeogProj-mnePy.py
@@ -10,9 +10,8 @@ import mne
 from mne import fiff
 from mne.fiff import proj
 import argparse 
-from mne.preprocessing import compute_proj_ecg, compute_proj_eog
-from mne.fiff.proj import make_projector, activate_proj
-from mne import Epochs
+from mne import preprocessing
+#from mne.fiff.proj import make_projector, activate_proj
 
 parser = argparse.ArgumentParser(description = "Get Input")
 parser.add_argument('subj', type = str)
@@ -29,23 +28,24 @@ proj_type = args.proj
 print proj
 
 dataPath = '/home/custine/MEG/data/'+exp+'/'+subjID+'/'
-raw_fname = dataPath + subjID + '_' +par+run+'_raw.fif'
+raw_fname = dataPath + subjID + '_' +par + run +'_raw.fif'
+raw_ssp_fname = dataPath + 'ssp/' + subjID + '_' +par + run + '_clean_'+proj_type+'_raw.fif'
 eve_fname = dataPath + 'ssp/'+ subjID + '_'  +par+run+'_'+proj_type+'-eve.fif'
 proj_fname = dataPath + 'ssp/' + subjID + '_' + par + run + '_' + proj_type + '-proj.fif'
 
 raw_in = fiff.Raw(raw_fname, preload = True)
-if proj_type =='eog': 
-    projs_eog, events = compute_proj_eog(raw_in, n_mag = 1, n_grad = 1, bads = [])
-    print projs_eog
-    projs_eog = fiff.proj.activate_proj(projs_eog)
-    fid = fiff.write.start_file(proj_fname)
-    print fid
-    fiff.write.write_proj(fid, projs_eog)
-    fiff.write.end_file(fid)
-    #projs_fname = fiff.proj.make_projector(projs_eog, raw_in.ch_names, bads = [])
-else:
-    projs_ecg, events = compute_proj_ecg(raw_in, chans= raw_in.ch_names, bads = [])
-    projs_ecg = fiff.proj.activate_proj(projs_ecg)
-    projs_ecg = fiff.proj.make_projector()
-    
 
+if proj_type =='eog': 
+    proj, eog_events = mne.preprocessing.compute_proj_eog(raw_in, n_grad = 1, n_mag = 1, tmin = -0.25, tmax = 0.25, event_id = 202, ch_name = 'EOG061') 
+    print eog_events     
+    mne.write_events(eve_fname, eog_events)    
+    raw_in.add_proj(proj, remove_existing = False)
+    raw_in.save(raw_ssp_fname, overwrite = True)
+    raw_in.apply_proj()
+else:
+    proj, ecg_events = mne.preprocessing.compute_proj_ecg(raw_in, n_grad = 1, n_mag = 1, tmin = -0.1, tmax = 0.1, event_id = 402, ch_name = 'ECG064') 
+    print ecg_events    
+    mne.write_events(eve_fname, ecg_events)    
+    raw_in.add_proj(proj, remove_existing = False)
+    raw_in.save(raw_ssp_fname, overwrite = True)
+    raw_in.apply_proj()
