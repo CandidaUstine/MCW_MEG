6f089f93d31343689d5683f45c7c546e5766ab98
CandidaUstine <cu38@cornell.edu>
2014-03-07 14:41:19 -0600

    old file

--- ssp_event_creator.py
+++ ssp_event_creator.py
@@ -1,26 +1,27 @@
 import os
 from os.path import join as pj
+from glob import glob 
 
-def ssp_event_creator(subjID, event)
+def ssp_event_creator(exp, subjID, event):
 
-fiffs = []
-in_path = pj('/home/custine/MEG/data/msabri',  ### TO CHANGE HERE
-fiffs = glob(pj(in_path,'*_raw.fif'))
-fiffs[:] = filter(lambda x: 'EmptyRoom' not in x and 'Spont' not in x, fiffs) ### TO Change here 
-fiffs.sort()
-if event == 'ecg': 
-  mlab = "ssp_find_ecg_event( '%s','%s');"
-else if event == 'eog':
-  mlab = "ssp_find_eog_event('%s');"
-
-cmd = ['warning off all']
-for fif in fiffs:
-     cmd.append(mlab %subjID %fif)
-cmd.append('exit;')
-ssp_dir = pj(in_path, '/', subjID, '/ssp/')
-if not os.path.isdir(ssp_dir):
-     os.mkdir(ssp_dir)
-helpers.write_file_with_list(pj(ssp_dir, 'reject.m'), cmd, data['verbose'])
+    fiffs = []
+    in_path = '/home/custine/MEG/data/'+ exp+'/'+subjID+'/'  ### TO CHANGE HERE
+    fiffs = glob(pj(in_path,'*_raw.fif'))
+    fiffs[:] = filter(lambda x: 'EmptyRoom' not in x and 'Spont' not in x, fiffs) ### TO Change here 
+    fiffs.sort()
+    if event == 'ecg': 
+      mlab = "ssp_find_ecg_event( '%s','%s');"
+    elif event == 'eog':
+      mlab = "ssp_find_eog_event('%s');"
+    
+    cmd = ['warning off all']
+    for fif in fiffs:
+         cmd.append(mlab %subjID %fif)
+    cmd.append('exit;')
+    ssp_dir = pj(in_path, '/', subjID, '/ssp/')
+    if not os.path.isdir(ssp_dir):
+         os.mkdir(ssp_dir)
+    write_file_with_list(pj(ssp_dir, 'reject.m'), cmd)
 
 
 def write_file_with_list(path,lines,quiet=False):
