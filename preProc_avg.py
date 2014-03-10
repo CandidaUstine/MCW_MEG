b6630f9eb849fc4ec92e5efbb8ca1141e0a66aa3
CandidaUstine <cu38@cornell.edu>
2014-03-10 16:21:20 -0500

    preProc_avg.py: 1) Run dict added and evoked is now appended for each run 2)exp name included as an arg input 3) mod.eve changed to .eve

--- preProc_avg.py
+++ preProc_avg.py
@@ -3,8 +3,8 @@
 Created on Thu Feb 13 16:59:34 2014
 
 @author: custine
-Usage: python preProc_avg.py subjID expName
-Example: python preProc_avg.py ac1 Left 
+Usage: python preProc_avg.py exp subjID expName
+Example: python preProc_avg.py custine ac1 Left 
 """
 
 import mne
@@ -18,17 +18,20 @@ import condCodes as cc
 #######Get Input ##
 
 parser = argparse.ArgumentParser(description='Get input')
+parser.add_argument('exp',type=str)
 parser.add_argument('subj',type=str)
-parser.add_argument('exp',type=str) ##Left, Right etc., 
+parser.add_argument('par',type=str) ##Left, Right etc., 
 args=parser.parse_args()
 subjID = args.subj
 exp = args.exp
-print subjID
+par = args.par
 print exp
+print subjID
+print par
 
 ########Analysis Parameters##
 ###Event file suffix 
-evSuffix = '_mod.eve'
+evSuffix = '.eve' ##changed from '_mod.eve'
 
 ###Projection and Average Reference 
 projVal = True
@@ -39,8 +42,9 @@ hp_cutoff = 0.7
 lp_cutoff = 20
 
 #######Experiment specific parameters 
-###EventLabels
-labelList = cc.condLabels[exp]
+###EventLabels and Runs
+runs = cc.runDict[par]
+labelList = cc.condLabels[par]
 event_id = {}
 condName = {}
 for row in labelList:
@@ -51,7 +55,7 @@ print event_id
 
 ###TimeWindow
 tmin = -.1
-tmax = float(cc.epMax[exp])
+tmax = float(cc.epMax[par])
 
 
 ########Artifact rejection parameters
@@ -66,47 +70,52 @@ gradFlat = 1000e-15
 ########Compute averages for each run
 
 evoked=[]
+evokedRuns =[]
 #print ev 
-
-##Setup Subject Speciifc Information
-data_path = '/home/custine/MEG/data/msabri/' +subjID
-event_file = data_path + '/eve/' + subjID + '_'+ exp + evSuffix
-print event_file
-
-raw_file = data_path +'/'+ subjID +'_' +exp + '_raw.fif' ##Change this suffix if you are using SSP 
-avgLog_file = data_path + '/ave_projon/logs/' +subjID + '_' + exp + '_py-ave.log'
-
-
-##Setup Reading fiff data structure
-print 'Reading Raw data... '
-raw = fiff.Raw(raw_file, preload = True)
-events = mne.read_events(event_file)
-
-raw_skip = raw.first_samp
-mne.set_log_file(fname = avgLog_file, overwrite = True)
-
-##Filter raw data 
-fiff.Raw.filter(raw, l_freq = hp_cutoff, h_freq = lp_cutoff)
-
-##Pick all channels 
-picks = []
-for i in range(raw.info['nchan']):
-    picks.append(i)
-    
-##Read Epochs and compute Evoked :) 
-print 'Reading Epochs from raw file...'
-epochs = mne.Epochs(raw, events, event_id, tmin, tmax, baseline = (None,0), picks = picks, proj = True, name = condName, preload = True, flat = dict(mag = magFlat, grad= gradFlat), reject=dict(mag=magRej, grad=gradRej))
-print epochs
-evoked = [epochs[cond].average(picks =picks) for cond in event_id]
-
-##Write Evoked 
-print 'Writing Evoked data to -ave.fif file...' 
-fiff.write_evoked(data_path + '/ave_projon/'+subjID+'_' +exp+'-ave.fif', evoked)
-
-print 'Completed! See ave.fif result in folder', data_path + '/ave_projon/'
+for run in runs:
+        print run
+        ##Setup Subject Speciifc Information
+        data_path = '/home/custine/MEG/data/'+exp+'/' +subjID
+        event_file = data_path + '/eve/' + subjID + '_'+ par +run+ evSuffix
+        print event_file
+        
+        raw_file = data_path +'/'+ subjID +'_' +par+run + '_raw.fif' ##Change this suffix if you are using SSP 
+        avgLog_file = data_path + '/ave_projon/logs/' +subjID + '_' + exp + '_py-ave.log'
+        print raw_file, avgLog_file
+        
+        ##Setup Reading fiff data structure
+        print 'Reading Raw data... '
+        raw = fiff.Raw(raw_file, preload = True)
+        events = mne.read_events(event_file)
+        
+        raw_skip = raw.first_samp
+        mne.set_log_file(fname = avgLog_file, overwrite = True)
+        
+        ##Filter raw data 
+        fiff.Raw.filter(raw, l_freq = hp_cutoff, h_freq = lp_cutoff)
+        
+        ##Pick all channels 
+        picks = []
+        for i in range(raw.info['nchan']):
+            picks.append(i)
+            
+        ##Read Epochs and compute Evoked :) 
+        print 'Reading Epochs from raw file...'
+        epochs = mne.Epochs(raw, events, event_id, tmin, tmax, baseline = (None,0), picks = picks, proj = True, name = condName, preload = True, flat = dict(mag = magFlat, grad= gradFlat), reject=dict(mag=magRej, grad=gradRej))
+        print epochs
+        evoked = [epochs[cond].average(picks =picks) for cond in event_id]
+        
+        ##Write Evoked 
+        print 'Writing Evoked data to -ave.fif file...' 
+        fiff.write_evoked(data_path + '/ave_projon/'+subjID+'_' +exp+'-ave.fif', evoked)
+        evokedRuns.append(evoked)
+        print 'Completed! See ave.fif result in folder', data_path + '/ave_projon/'
 
 ##Show the Result - Plotting the evoked data
 #mne.viz.plot_evoked(evoked, exclude = [])
+
+print len(evokedRuns)
+
 
 
 
