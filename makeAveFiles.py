2adfc01f06ade375a7a9dd613431d167943911b2
CandidaUstine <cu38@cornell.edu>
2014-03-10 16:17:30 -0500

    makAveFiles.py: 1) Included exp tag in argument input syntax, 2)included run dictionary 3)using .eve indtead of mod.eve files

--- makeAveFiles.py
+++ makeAveFiles.py
@@ -14,9 +14,9 @@ import sys
 import os
 import condCodes as cc 
 
-def makeAveFiles(subjID, projType):
+def makeAveFiles(exp, subjID, projType):
     
-    filePrefix = '/home/custine/MEG/data/msabri/' + subjID
+    filePrefix = '/home/custine/MEG/data/' + exp + '/' + subjID
     
     gradRej = "2000e-13"
     magRej = "3000e-15"
@@ -25,41 +25,47 @@ def makeAveFiles(subjID, projType):
     gradFlat = "1000e-15"
     eegFlat = "0"
     
-    expList = ['Left', 'LeftDual', 'Right', 'RightDual']
+    if exp == 'msabri':    
+        parList = ['Left', 'LeftDual', 'Right', 'RightDual']
+    elif exp == 'custine':
+        parList = ['Audio']
+        runDict = {'Audio':['Run1', 'Run2']}
+        
     condDict = cc.condLabels
     epMaxDict = cc.epMax
     
-    for exp in expList: 
-        filename = filePrefix + '/ave/' + subjID + '_' + exp + '.ave'
-        print filename
-        myFile = open(filename, "w")
-        myFile.close()
-        
-        myFile = open(filename, "a")
-        myFile.write('average{\n')
-        myFile.write('\tname\t\"'+exp+' Averages\"\n')
-        myFile.write('\toutfile\t\t'+subjID+'_'+exp+'-ave.fif\n')
-        myFile.write('\tlogfile\t\t'+filePrefix+'/logs/'+subjID+'_'+exp+'-ave.log\n')
-        
-        if projType == 'projon':
-            myFile.write('\teventfile\t'+filePrefix+'/eve/'+subjID+'_mod.eve\n\n')
-            myFile.write('\tgradReject\t'+gradRej+'\n')
-            myFile.write('\tmagReject\t'+magRej+'\n')
-            myFile.write('\teegReject\t'+eegRej+'\n')
-            myFile.write('\tgradFlat\t'+gradFlat+'\n')
-            myFile.write('\tmagFlat\t\t'+magFlat+'\n')            
-            myFile.write('\teegFlat\t\t'+eegFlat+'\n\n')
+    for par in parList: 
+        for run in runDict[par]:
+            filename = filePrefix + '/ave/' + subjID + '_' + par+ run + '.ave'
+            print filename
+            myFile = open(filename, "w")
+            myFile.close()
             
-        for item in condDict[exp]:
-            myFile.write('\tcategory {\n')
-            myFile.write('\t\tname\t'+item[1]+'\n')
-            myFile.write('\t\tevent\t'+item[0]+ '\n')
-            myFile.write('\t\ttmin\t-0.1\n')
-            myFile.write('\t\ttmax\t'+epMaxDict[exp]+'\n\t}\n\n')
+            myFile = open(filename, "a")
+            myFile.write('average{\n')
+            myFile.write('\tname\t\"'+par+' Averages\"\n')
+            myFile.write('\toutfile\t\t'+subjID + '_'+ par + run +'-ave.fif\n')
+            myFile.write('\tlogfile\t\t'+filePrefix+'/logs/'+subjID+'_'+par+run+'-ave.log\n')
             
-        myFile.write('}\n')
+            if projType == 'projon':
+                myFile.write('\teventfile\t'+filePrefix+'/eve/'+subjID+'_'+par+run+'.eve\n\n')
+                myFile.write('\tgradReject\t'+gradRej+'\n')
+                myFile.write('\tmagReject\t'+magRej+'\n')
+                myFile.write('\teegReject\t'+eegRej+'\n')
+                myFile.write('\tgradFlat\t'+gradFlat+'\n')
+                myFile.write('\tmagFlat\t\t'+magFlat+'\n')            
+                myFile.write('\teegFlat\t\t'+eegFlat+'\n\n')
+                
+            for item in condDict[par]:
+                myFile.write('\tcategory {\n')
+                myFile.write('\t\tname\t'+item[1]+'\n')
+                myFile.write('\t\tevent\t'+item[0]+ '\n')
+                myFile.write('\t\ttmin\t-0.1\n')
+                myFile.write('\t\ttmax\t'+epMaxDict[par]+'\n\t}\n\n')
+                
+            myFile.write('}\n')
             
         
     
 if __name__ == "__main__":
-    makeAveFiles(sys.argv[1], sys.argv[2])
+    makeAveFiles(sys.argv[1], sys.argv[2], sys.argv[3])
