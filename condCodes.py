a9b93f8bbb3b4108b58b1975039e933c3bf997d2
CandidaUstine <cu38@cornell.edu>
2014-03-10 15:32:30 -0500

    Included custine experiment conditions

--- condCodes.py
+++ condCodes.py
@@ -1,7 +1,8 @@
 codes = {'Left':('3', '4', '100', '200', '400', '500'),
          'Right':('3', '4', '100', '200', '400', '500'),
          'LeftDual':('3', '4', '100', '200', '400', '500'),
-         'RightDual':('3', '4', '100', '200', '400', '500')
+         'RightDual':('3', '4', '100', '200', '400', '500'),
+         'Audio':('1', '2', '64', '128')
          }
 
 
@@ -32,11 +33,17 @@ condLabels = {'Left':
                     ['100', 'standard_corr'],
                     ['200', 'standard_incorr'],
                     ['400', 'standard_dev_seq_corr'],
-                    ['500', 'standard_dev_seq_incorr']]
+                    ['500', 'standard_dev_seq_incorr']],
+               'Audio':
+                    [['1', 'std_tone'],
+                     ['2', 'dev_tone'],
+                     ['64', 'dev_detected'],
+                     ['128', 'std_detected']]
                     }
             
 epMax = {'Left': '.4',
          'Right': '.4',
          'LeftDual': '.4',
-         'RightDual': '.4'
+         'RightDual': '.4',
+         'Audio': '0.4'
         }
