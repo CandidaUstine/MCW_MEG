2f5a623b595d5a43e608de9302c3b1cc974d87eb
CandidaUstine <cu38@cornell.edu>
2014-04-02 15:59:20 -0500

    First commit with comments

--- makeInv.sh
+++ makeInv.sh
@@ -23,11 +23,14 @@ cd $subj_dir/ave_projon/
 date >>&$log 
 setenv SUBJECT $2 
 
+##Create Inverse Solutions 
 foreach t ('meg')
 	mne_do_inverse_operator --fwd $2_Audio_All-ave-7-$t-fwd.fif --depth --loose 0.5 --$t --senscov $2_Audio_All-cov.fif --inv $2_Audio_All-ave-7-$t-inv.fif >>&$log 
 end 
 
-
+##Create Morph maps between the subject space and fsaverage 
 mne_make_morph_maps --from $2 --to fsaverage >>&$log 
 
 date >>&$log 
+
+echo "Finished. Check the Inv solutions in the subject's ave_projon folder" 
