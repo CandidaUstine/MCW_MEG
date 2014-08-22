70c7c02ee73e8c47a6e535b19e6ab59c8c29fe1c
CandidaUstine <cu38@cornell.edu>
2014-08-22 16:49:22 -0500

    Edited to treat entire time frame as one sample for fiff write back purpose

--- krns_kr3/preProc_project_allC_FT.m
+++ krns_kr3/preProc_project_allC_FT.m
@@ -1,4 +1,4 @@
-function preProc_project_allC_FT(exp, subjID, sessID, run)
+function preProc_project_allC_FT(subjID, sessID, run)
 
 %Wrapper Script to run the ICA analysis on the Neuromag Raw.fif input file.
 %Computes the first 20 ICA componenets of the specified input and plots the
@@ -9,15 +9,15 @@ function preProc_project_allC_FT(exp, subjID, sessID, run)
 
 %Author: Candida Jane Maria Ustine, custine@mcw.edu
 %Modified for krns_kr3 study on 07/29/2014 
-%Usage: preProc_project_FT('krns_kr3', '9444', 's1', 'run1', 100)
+%Usage: preProc_project_allC_FT('9444', 's1', 'run1')
 
 %% Initialise Fieldtrip Defaults 
 ft_defaults
 
 %% Initialise Subject Specific Defaults  
-inpath = ['/home/custine/MEG/data/',exp,'/', subjID, '/', sessID, '/'];
+inpath = ['/home/custine/MEG/data/krns_kr3/', subjID, '/', sessID, '/'];
 fiff = strcat(inpath, subjID,'_',sessID, '_', run, '_raw.fif')
-comp_file = strcat(inpath,'ssp/fieldtrip/', subjID,'_',run,'_allC_comp.mat')
+comp_file = strcat(inpath,'ssp/fieldtrip/', subjID,'_',sessID,'_', run,'_allC_comp.mat')
 
 Fevents = ft_read_event(fiff);
 [len, ~] = size(Fevents);
@@ -29,26 +29,26 @@ for i = 1:len
        F = [F, Fevents(i).sample];
 end
 
-% %For the whole time window of the raw file 
-F = F'
-[len, ~] = size(F);
-begS = F(1,1)
-endS = F(len,1)
-samples = horzcat(begS, endS, 0)
-
-% %Short samples... 
-% F = F';
+% % %For the whole time window of the raw file (same as Option 2 below) 
+% F = F'
 % [len, ~] = size(F);
-% begS = F(:,1);
-% begS = begS(1:len-1);
-% for i = 1:size(begS)-1;
-%     endS(i) = (begS(i+1)); %%1118 samples
-% end
-% endS(i+1) = endS(i) +10000
-% offset = zeros(1,len-1);
-% size(endS)
-% size(begS)
-% samples = horzcat(begS, endS', offset')
+% begS = F(1,1)
+% endS = F(len,1)
+% samples = horzcat(begS, endS, 0)
+
+%Short samples - all trials... 
+F = F';
+[len, ~] = size(F);
+begS = F(:,1);
+begS = begS(1:len-1);
+for i = 1:size(begS)-1;
+    endS(i) = (begS(i+1)); %%1118 samples
+end
+endS(i+1) = endS(i) +10000
+offset = zeros(1,len-1);
+size(endS)
+size(begS)
+samples = horzcat(begS, endS', offset')
 
 % % %% OPTION 2: Keep this (and comment out OPTION 1) If you are writing the data structure after ICA analysis back to a fiff file
 % % for i = 1:len
@@ -59,7 +59,13 @@ samples = horzcat(begS, endS, 0)
 % % begSt = F(1) 
 % % endSt = F(len)
 % % offsett = 0
-% % samples = horzcat(begSt, endSt, offsett)
+% % samples = horzcat(begSt, endSt, offsett)% % the original data can now be reconstructed, excluding those components
+% load(comp_file)
+% cfg = [];
+% cfg.component = [8 10];
+% data_clean = ft_rejectcomponent(cfg, comp, data); %using the non sampled data :) (use the sampled data when you want to write back to a fif) 
+% save(comp_file, 'data_clean', 'comp', 'data', 'cfg')
+
 
 %% Define Trials 
 dat = ft_read_data(fiff);
@@ -97,12 +103,12 @@ data = ft_preprocessing(cfg)
 % %Reject Visual Trials and see the epoched response: (Optional) 
 % data_clean   = ft_rejectvisual(cfg, data);
 
-% %you should downsample your data before continuing, otherwise ICA decomposition will take too long
-data_orig = data
-cfg = []
-cfg.resamplefs = 300
-cfg.detrend = 'no'
-data = ft_resampledata(cfg, data)
+% % %you should downsample your data before continuing, otherwise ICA decomposition will take too long
+% data_orig = data
+% cfg = []
+% cfg.resamplefs = 300
+% cfg.detrend = 'no'
+% data = ft_resampledata(cfg, data)
 
 %% Set the ICA method 
 cfg            = []; % the original data can now be reconstructed, excluding those components
@@ -132,11 +138,12 @@ ft_databrowser(cfg, comp)
  
 save(comp_file, 'cfg', 'data', 'comp','comp_file')
 
-% 
-% % the original data can now be reconstructed, excluding those components
-% cfg = [];
-% cfg.component = [3 6];
-% data_clean = ft_rejectcomponent(cfg, comp,data); %using the sampled data :) 
-% save(comp_file, 'data_clean', 'comp')
+
+% the original data can now be reconstructed, excluding those components
+% load(comp_file)
+cfg = [];
+cfg.component = [8 13];
+data_clean = ft_rejectcomponent(cfg, comp, data); %using the non sampled data :) (use the sampled data when you want to write back to a fif) 
+save(comp_file, 'data_clean', 'comp', 'data', 'cfg')
 
 end
