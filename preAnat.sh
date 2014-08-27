351cdc911033d3df9c35c843ce5124bb63940762
CandidaUstine <cu38@cornell.edu>
2014-08-27 16:13:59 -0500

    Modified to run for krns-kr3 study as well - inputs changed

--- preAnat.sh
+++ preAnat.sh
@@ -4,38 +4,57 @@
 #@author: custine
 #Usage: ./preAnat.sh exp subjID
 
-date
+date 
 
 setenv SUBJECT $2
 set BEM_METHOD = WATER 
-set subj_dir = /home/custine/MRI/structurals/subjects/$2/
+set surf_dir = '/home/custine/MRI/structurals/subjects/$2/'
+
+if ($1 == 'krns_kr3') then 
+    echo "Study Name: " 
+    echo $1 
+    set surf_dir = /mnt/file1/binder/KRNS/anatomies/surfaces
+    set subj_dir = /home/custine/MEG/data/krns_kr3/$1/$2/
+    setenv SUBJECTS_DIR $surf_dir
+endif
+set log = /home/custine/MEG/data/$1/$2/logs/$2_preAnat.log
 
 ##remove the existing log file
-if ( -e /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log ) then
-    rm /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log
+if ( -e $log ) then
+    rm $log
 endif
 
+echo "Jane here"
+date >>& $log
+
+echo $surf_dir 
+echo $subj_dir
+echo $log
+
 if ( $#argv == 3 ) then
-    #set log = '/home/custine/MEG/data/{$1}/{$2}/ave_projon/logs/preProc_avg.log'
-    touch /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log
-    echo "Logging to default log..." >>& /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log
+    touch $log
+    echo "Logging to default log..." >>& $log
 endif
 
 echo "Hello! Make sure the Cortical surface reconstructions have been created for your subject before you run this step..." 
+echo "Hello! Make sure the Cortical surface reconstructions have been created for your subject before you run this step..." >>& $log
 
 #Setting up the anatomical MR images for MRILab 
-echo "Setting up the anatomical MR images for MRILab..."
-mne_setup_mri --overwrite >>& /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log 
+echo "Setting up the anatomical MR images for MRILab..." 
+mne_setup_mri --overwrite >>& $log 
 
 #Setting up the Source Space(creating a decimated dipole grid on the wm surface - default 7mm grid spacing)
 echo "Setting up the Source Space..."
-mne_setup_source_space >>& /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log 
+mne_setup_source_space >>& $log 
 
 #Creating the BEM Model meshes 
 echo "Creating the BEM meshes..."
 echo "Using the Watershed method to create the surfaces"
-mne_watershed_bem --overwrite >>& /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log
-cd /home/custine/MRI/structurals/subjects/$2/bem
+mne_watershed_bem --overwrite >>& $log
+cd $surf_dir/$2/bem
+
+ls
+ 
 rm *.surf
 ln -s ./watershed/$2_outer_skin_surface outer_skin.surf
 ln -s ./watershed/$2_outer_skull_surface outer_skull.surf
@@ -43,8 +62,9 @@ ln -s ./watershed/$2_inner_skull_surface inner_skull.surf
 #mv $SUBJECTS_DIR/$1/bem/$1-bem.fif $SUBJECTS_DIR/$1/bem/$1-orig-bem.fif
 
 #Setting up the BEM surfaces for viewing 
-mne_setup_forward_model --surf --ico 4  >>& /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log ##IF surfaces are not contained within each other in the proper manner then use the --innershift and --outershift tags in order to modify the surfaces while creating the forward model. 
-mne_make_scalp_surfaces >>& /home/custine/MEG/data/{$1}/{$2}/logs/preAnat.log
-mv /home/custine/MRI/structurals/subjects/$2/$2-head.fif /home/custine/MRI/structurals/subjects/$2/$2-head-old.fif 
-ln /home/custine/MRI/structurals/subjects/$2/$2-head-medium.fif /home/custine/MRI/structurals/subjects/$2/$2-head.fif
- 
+mne_setup_forward_model --surf --ico 4  >>& $log ##IF surfaces are not contained within each other in the proper manner then use the --innershift and --outershift tags in order to modify the surfaces while creating the forward model. 
+mne_make_scalp_surfaces >>& $log
+mv $surf_dir/$2/bem/$2-head.fif $surf_dir/$2/bem/$2-head-old.fif 
+ln $surf_dir/$2/bem/$2-head-medium.fif $surf_dir/$2/bem/$2/$2-head.fif
+
+
