4fde33e0025167906b1635967af793f734e7b44b
CandidaUstine <cu38@cornell.edu>
2014-09-10 14:56:34 -0500

    edited to create cov matrices from raw file(for emptyroom) and from epoched data(for individual runs) using a baseline

--- krns_kr3/preProc_cov.py
+++ krns_kr3/preProc_cov.py
@@ -5,8 +5,8 @@ Created on Wed Aug 27 14:52:52 2014
 Estimate covariance matrix from a raw FIF file
 ==============================================
 
-Usage: python preProc_avg.py subjID sessID eve
-Example: python preProc_avg.py 9511 s1 Word
+Usage: python preProc_cov.py subjID sessID eve
+Example: python preProc_cov.py 9511 s1 Word
 
 """
 # Author: Alexandre Gramfort <alexandre.gramfort@telecom-paristech.fr>
@@ -19,8 +19,6 @@ import mne
 from mne import io
 import argparse
 import condCodes as cc
-import copy
-import numpy
 
 parser = argparse.ArgumentParser(description='Get input')
 parser.add_argument('subj',type=str)
@@ -48,7 +46,7 @@ for row in labelList:
 print event_id
 
 #runs = cc.runDict['Word']
-runs = ['run1', 'run2'] ###For other runs use the baseline and set tmin/tmax using epoched data. 
+runs = ['emptyroom'] #, 'run1', 'run2'] ###For other runs use the baseline and set tmin/tmax using epoched data. 
 #print runs
 covRuns = []
 for runID in runs:
@@ -56,6 +54,7 @@ for runID in runs:
         
         data_path = '/home/custine/MEG/data/krns_kr3/' +subjID+'/'+sessID
         fname = data_path +'/'+ subjID + '_'+ sessID +'_'+runID +'_raw.fif'
+        print fname
         cname = data_path +'/cov/'+ subjID + '_'+ sessID +'_' + runID + '-cov.fif'
         covLog_file = data_path + '/cov/logs/' +subjID + '_' + sessID+ '_'+runID + '_cov.log'
         event_file = data_path + '/eve/triggers/' + subjID + '_'+ sessID +'_'+runID +'_' + eve_file
@@ -64,23 +63,28 @@ for runID in runs:
         print covLog_file
         print 'Reading Raw data... '
         raw = io.Raw(fname)
-        events = mne.read_events(event_file)
-        include = []  # or stim channels ['STI 014']
-        #raw.info['bads'] += ['EEG 053']  # bads + 1 more
-        # pick EEG channels
-        picks = mne.pick_types(raw.info, meg=True, eeg=True, stim=False, eog=True, include=include, exclude='bads')
-        reject = dict(mag = 4e-12, grad = 4000e-13)
-        epochs = mne.Epochs(raw, events, event_id, tmin, tmax, baseline = (None,0), picks = picks, proj = True, preload = True, reject=reject)
-        print epochs
-        ###Example: reject = dict(grad=4000e-13, # T / m (gradiometers)
-        #              mag=4e-12, # T (magnetometers)
-        #              eeg=40e-6, # uV (EEG channels)
-        #              eog=250e-6 # uV (EOG channels)
-        #              )
-        
-        # Compute the covariance from the raw data
-        cov = mne.compute_covariance(epochs, tmin = None, tmax = 0)
-        print cov
+        if runID == "emptyroom":
+            cov = mne.compute_raw_data_covariance(raw) #, tmin = None, tmax = 0) #, reject = None, picks = picks)
+            print cov
+        else:
+            
+            events = mne.read_events(event_file)
+            include = []  # or stim channels ['STI 014']
+    #        #raw.info['bads'] += ['EEG 053']  # bads + 1 more
+    #        # pick EEG channels
+            picks = mne.pick_types(raw.info, meg=True, eeg=False, stim=False, eog=True, include=include, exclude='bads')
+            reject = dict(mag = 4e-12, grad = 4000e-13)
+            epochs = mne.Epochs(raw, events, event_id, tmin, tmax, baseline = (None,0), picks = picks, proj = True, preload = True, reject=reject)
+            print epochs
+#            ##Example: reject = dict(grad=4000e-13, # T / m (gradiometers)
+#                          mag=4e-12, # T (magnetometers)
+#                          eeg=40e-6, # uV (EEG channels)
+#                          eog=250e-6 # uV (EOG channels)
+#                          )
+            
+#             Compute the covariance from the raw data or from epochs 
+            cov = mne.compute_covariance(epochs, tmin = None, tmax = 0) #Use for runs... 
+            print cov
 #        covRuns.append(cov)
         mne.write_cov(cname, cov)
 #        
