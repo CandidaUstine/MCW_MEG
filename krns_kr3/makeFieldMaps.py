f53074aa9eb19f1dd568fb240bdd9d1cfbacbdee
CandidaUstine <cu38@cornell.edu>
2014-09-10 15:00:39 -0500

    first commit

--- krns_kr3/makeFieldMaps.py
+++ krns_kr3/makeFieldMaps.py
@@ -16,6 +16,7 @@ from mne import make_field_map, read_evoked
 
 
 parser = argparse.ArgumentParser(description='Get input')
+parser.add_argument('ave_name', type = str)
 parser.add_argument('set',type=int)
 parser.add_argument('time1',type=float)
 parser.add_argument('time2',type=float)
@@ -25,7 +26,7 @@ args=parser.parse_args()
 ##script for creating average field map
 
 data_path = '/home/custine/MEG/data/krns_kr3/9367/s5/'
-evoked_fname = data_path + 'ave_projon/9367_s5_Word_All-ave.fif'
+evoked_fname = data_path + 'ave_projon/9367_s5_Noun_Place_All-ave.fif'
 subjects_dir = '/mnt/file1/binder/KRNS/anatomies/surfaces/'
 trans_fname = '/mnt/file1/binder/KRNS/anatomies/surfaces/9367/mri/T1-neuromag/sets/COR-custine-140827-120918.fif'
 # If trans_fname is set to None then only MEG estimates can be visualized
@@ -41,7 +42,7 @@ maps = make_field_map(evoked, trans_fname=trans_fname, subject='9367',
 # explore several points in time
 [evoked.plot_field(maps, time=time) for time in [0.09, .11]]
 
-evoked.save('/home/custine/MEG/data/krns_kr3/9367/s5/results/fieldmaps/9367_s5_Word_All-'+'-'+str(args.time1)+'-'+str(args.time2)+'-ave.fif')
+evoked.save('/home/custine/MEG/data/krns_kr3/9367/s5/results/fieldmaps/9367_s5_' + str(args.ave_name) + '_All-'+'-'+str(args.time1)+'-'+str(args.time2)+'-ave.fif')
 #evoked.save(data_path + args.prefix+'-'+str(args.set)+'-'+str(args.time1)+'-'+str(args.time2)+'-ave.fif')
 
 # evoked = mne.fiff.read_evoked('ga_BaleenHP_All_meg-n24-goodC-ave.fif',setno=6)
