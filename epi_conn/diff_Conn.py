17c521032c43fd2eb20ebf9c84f6d0d0239badec
CandidaUstine <cu38@cornell.edu>
2014-10-16 16:37:29 -0500

    plotting hotelling t squared stats

--- epi_conn/diff_Conn.py
+++ epi_conn/diff_Conn.py
@@ -49,7 +49,6 @@ diff_con = con_gp1 - con_gp2
 np.savetxt(conAll_diff_matrix_fname, diff_con)
 ##############################################################################################
 ##############################################################################################
-######################### 2 sample T test ##########################################################
 #if group == 'Left':
 lsubj1 = 'EP1'
 lsubj2 = 'EP3'
@@ -78,52 +77,64 @@ rcon_res1 = np.loadtxt(rcon1_fname, delimiter = ',')
 rcon_res2 = np.loadtxt(rcon2_fname, delimiter = ',')
 rcon_res3 = np.loadtxt(rcon3_fname, delimiter = ',')
 rcon_res4 = np.loadtxt(rcon4_fname, delimiter = ',')
+######################### 2 sample T test ##########################################################
+#conAll_T_matrix_fname = data_path + 'TStat_GrandAvgConnectivityMatrix_' + freq + '_' + gp1 + '-' + gp2 + '.txt'
+#conAll_T_plot_fname = data_path + 'TStat_GrandAvgConnectivityPlot_' + freq + '_' + gp1 + '-' + gp2 + '.png'
+#
+#con_t = np.empty((68,68))
+#con_t.fill(0)
+#for i in range (0, 68):
+#    for j in range(0,68):
+#        l_mean = (lcon_res1[i,j] + lcon_res2[i,j] + lcon_res3[i,j] + lcon_res4[i,j] )/4
+#        r_mean = (rcon_res1[i,j] + rcon_res2[i,j] + rcon_res3[i,j] + rcon_res4[i,j] )/4
+#        l_a = [lcon_res1[i,j], lcon_res2[i,j], lcon_res3[i,j], lcon_res4[i,j]]
+#        l_std = float(np.std(l_a))
+#        #print l_std
+#        r_a = [rcon_res1[i,j], rcon_res2[i,j], rcon_res3[i,j], rcon_res4[i,j]]
+#        r_std = float(np.std(r_a))
+#        #print r_std
+#        l_var = pow(l_std, 2)
+#        r_var = pow(r_std, 2)
+#        #print l_var, r_var
+#        if l_mean == 0 and r_mean == 0:
+#            t = float(0)
+#        else:
+#            s_lr = np.sqrt((l_var + r_var)/2)
+#            s_lr = float(s_lr * np.sqrt(0.5))
+#            t = (l_mean - r_mean)
+#         #   print s_lr
+#            t = t/s_lr
+#        #print t 
+#        con_t[i,j] = t 
+#    print 
+#print con_t 
+#print con_t.T
+#np.savetxt(conAll_T_matrix_fname, con_t)
+####################################################################################################
+######################### 2 sample T test - Hotelling T squared statistic  ##########################################################
+tsq_matrix_fname = '/home/custine/MEG/results/source_level/ConnectivityPlots/Hotel-Tsq_GrandAvgConnectivityMatrix_' + freq + '_' + gp1 + '-' + gp2 + '.txt'
+tsq_plot_fname = '/home/custine/MEG/results/source_level/ConnectivityPlots/Hotel-Tsq_GrandAvgConnectivityPlot_' + freq + '_' + gp1 + '-' + gp2 + '.png'
 
-conAll_T_matrix_fname = data_path + 'TStat_GrandAvgConnectivityMatrix_' + freq + '_' + gp1 + '-' + gp2 + '.txt'
-conAll_T_plot_fname = data_path + 'TStat_GrandAvgConnectivityPlot_' + freq + '_' + gp1 + '-' + gp2 + '.png'
-
-con_t = np.empty((68,68))
-con_t.fill(0)
-for i in range (0, 68):
-    for j in range(0,68):
-        l_mean = (lcon_res1[i,j] + lcon_res2[i,j] + lcon_res3[i,j] + lcon_res4[i,j] )/4
-        r_mean = (rcon_res1[i,j] + rcon_res2[i,j] + rcon_res3[i,j] + rcon_res4[i,j] )/4
-        l_a = [lcon_res1[i,j], lcon_res2[i,j], lcon_res3[i,j], lcon_res4[i,j]]
-        l_std = float(np.std(l_a))
-        #print l_std
-        r_a = [rcon_res1[i,j], rcon_res2[i,j], rcon_res3[i,j], rcon_res4[i,j]]
-        r_std = float(np.std(r_a))
-        #print r_std
-        l_var = pow(l_std, 2)
-        r_var = pow(r_std, 2)
-        print l_var, r_var
-        if l_mean == 0 and r_mean == 0:
-            t = float(0)
-        else:
-            s_lr = np.sqrt((l_var + r_var)/2)
-            s_lr = float(s_lr * np.sqrt(0.5))
-            t = (l_mean - r_mean)
-            print s_lr
-            t = t/s_lr
-        print t 
-        con_t[i,j] = t 
-    print 
-print con_t 
-np.savetxt(conAll_T_matrix_fname, con_t)
+tsq = np.loadtxt(tsq_matrix_fname, delimiter = ' ')
+print tsq
+     
 ##############################################################################################
 ##############################################################################################
-###############Plotting the Conn##################
+###############Plotting the Conn - Diff or T test stats##################
 labels, label_colors = mne.labels_from_parc('fsaverage', parc='aparc', subjects_dir=subjects_dir) ##or use read_labels_from_annot() 
-print labels
+#print labels
 print
 
 #### Now, we visualize the connectivity using a circular graph layout
 # First, we reorder the labels based on their location in the left hemi
 label_names = [label.name for label in labels]
-print label_names[:-1] #### TO GET RID OF UNKNOWN LABEL.LH 
+#print label_names[:-1] #### TO GET RID OF UNKNOWN LABEL.LH 
 label_names = label_names[:-1]
+for name in label_names:
+    print name
+np.savetxt('/home/custine/MEG/results/source_level/ConnectivityPlots/label_names.txt', label_names)  
 lh_labels = [name for name in label_names if name.endswith('lh')]
-print len(lh_labels)
+#print len(lh_labels)
 
 # Get the y-location of the label
 label_ypos = list()
@@ -131,51 +142,53 @@ for name in lh_labels:
     idx = label_names.index(name)
     ypos = np.mean(labels[idx].pos[:, 1])
     label_ypos.append(ypos)
-print len(label_ypos)
+#print len(label_ypos)
 
 # Reorder the labels based on their location
 lh_labels = [label for (ypos, label) in sorted(zip(label_ypos, lh_labels))]
 
 # For the right hemi
 rh_labels = [label[:-2] + 'rh' for label in lh_labels]
-print rh_labels
+#print rh_labels
 
 # Save the plot order and create a circular layout
 node_order = list()
 node_order.extend(lh_labels[::-1])  #reverse the order
 node_order.extend(rh_labels)
 
+print 
+print 'Jane Here' 
+print 
+
+print node_order
+print len(node_order)
+
 node_angles = circular_layout(label_names, node_order, start_pos=90,
                               group_boundaries=[0, len(label_names) / 2])
+                              
+###############################################################################################
+##########Plot the difference 
+###############3
+## Plot the graph using node colors from the FreeSurfer parcellation. We only
+## show the 300 strongest connections.
+#plot_connectivity_circle(diff_con, label_names, n_lines=300,
+#                         node_angles=node_angles, node_colors=label_colors,vmin = 0.0, vmax = 1.0,
+#                         title='Coherence - Difference ('+ gp1 + '-' +gp2 +') - '+ freq)
+#import matplotlib.pyplot as plt
+#plt.savefig(conAll_diff_plot_fname, facecolor='black')
 
-# Plot the graph using node colors from the FreeSurfer parcellation. We only
-# show the 300 strongest connections.
-plot_connectivity_circle(con_t, label_names, n_lines=300,
-                         node_angles=node_angles, node_colors=label_colors,
-                         title='Coherence - T stats ('+ gp1 + '-' +gp2 +') - '+ freq)
-import matplotlib.pyplot as plt
-plt.savefig(conAll_T_plot_fname, facecolor='black')
-#plt.show()
+###########################################################################################
+##########Plot the connectivity T stats - 2 sample
+## Plot the graph using node colors from the FreeSurfer parcellation. We only
+## show the 300 strongest connections.
+#plot_connectivity_circle(con_t, label_names, n_lines=300,
+#                         node_angles=node_angles, node_colors=label_colors,
+#                         title='Coherence - T stats ('+ gp1 + '-' +gp2 +') - '+ freq)
+#import matplotlib.pyplot as plt
+#plt.savefig(conAll_T_plot_fname, facecolor='black')
+##plt.show()
+#
+##################################################################################################3                              
 
-################
-#########Plot the difference 
-##############3
-# Plot the graph using node colors from the FreeSurfer parcellation. We only
-# show the 300 strongest connections.
-plot_connectivity_circle(diff_con, label_names, n_lines=300,
-                         node_angles=node_angles, node_colors=label_colors,vmin = 0.0, vmax = 1.0,
-                         title='Coherence - Difference ('+ gp1 + '-' +gp2 +') - '+ freq)
-import matplotlib.pyplot as plt
-plt.savefig(conAll_diff_plot_fname, facecolor='black')
 
-## Plot connectivity for both methods in the same plot
-#fig = plt.figure(num=None, figsize=(8, 4), facecolor='black')
-#no_names = [''] * len(label_names)
-#for ii, method in enumerate(con_methods):
-#    plot_connectivity_circle(con_res[method], no_names, n_lines=300,
-#                             node_angles=node_angles, node_colors=label_colors,
-#                             title=method, padding=0, fontsize_colorbar=6,
-#                             fig=fig, subplot=(1, 2, ii + 1))
-#plt.savefig('/home/custine/MEG/data/epi_conn/' + subj + '/coh/' + subj + '_circle_coh_imcoh_' + freq + '.png', facecolor='black')
-#plt.show()
-    
+
