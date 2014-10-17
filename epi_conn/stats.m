07fba0f77f1ebe620a5c41f6c7c0795018474831
CandidaUstine <cu38@cornell.edu>
2014-10-17 11:56:38 -0500

    hotelling t squared

--- epi_conn/stats.m
+++ epi_conn/stats.m
@@ -4,20 +4,21 @@ function stats(freq)
 %  http://en.wikipedia.org/wiki/Hotelling's_T-squared_distribution#Hotelling.27s_two-sample_T-squared_statistic
 % Left vs Right each with 68 regions, 4 subject groups in each grup.
 
-L1 = dlmread(strcat('/home/custine/MEG/data/epi_conn/EP1/coh/EP1_', freq, '_subj_connectivityMatrix.txt'));
-L2 = dlmread(strcat('/home/custine/MEG/data/epi_conn/EP3/coh/EP3_', freq, '_subj_connectivityMatrix.txt'));
-L3 = dlmread(strcat('/home/custine/MEG/data/epi_conn/EP5/coh/EP5_', freq, '_subj_connectivityMatrix.txt'));
-L4 = dlmread(strcat('/home/custine/MEG/data/epi_conn/EP7/coh/EP7_', freq, '_subj_connectivityMatrix.txt'));
+L1 = dlmread(strcat('/home/custine/MEG/data/epi_conn/EP1/coh/EP1_', freq, '_subj_plv_ConnectivityMatrix.txt'));
+L2 = dlmread(strcat('/home/custine/MEG/data/epi_conn/EP3/coh/EP3_', freq, '_subj_plv_ConnectivityMatrix.txt'));
+L3 = dlmread(strcat('/home/custine/MEG/data/epi_conn/EP5/coh/EP5_', freq, '_subj_plv_ConnectivityMatrix.txt'));
+L4 = dlmread(strcat('/home/custine/MEG/data/epi_conn/EP7/coh/EP7_', freq, '_subj_plv_ConnectivityMatrix.txt'));
 L = cat(3, L1, L2, L3, L4);
 
 
-R1 = dlmread(strcat('/home/custine/MEG/data/epi_conn/EP4/coh/EP4_', freq, '_subj_connectivityMatrix.txt'));
-R2 = dlmread(strcat('/home/custine/MEG/data/epi_conn/EP6/coh/EP6_', freq, '_subj_connectivityMatrix.txt'));
-R3 = dlmread(strcat('/home/custine/MEG/data/epi_conn/EP8/coh/EP8_', freq, '_subj_connectivityMatrix.txt'));
-R4 = dlmread(strcat('/home/custine/MEG/data/epi_conn/EP10/coh/EP10_', freq, '_subj_connectivityMatrix.txt'));
+R1 = dlmread(strcat('/home/custine/MEG/data/epi_conn/EP4/coh/EP4_', freq, '_subj_plv_ConnectivityMatrix.txt'));
+R2 = dlmread(strcat('/home/custine/MEG/data/epi_conn/EP6/coh/EP6_', freq, '_subj_plv_ConnectivityMatrix.txt'));
+R3 = dlmread(strcat('/home/custine/MEG/data/epi_conn/EP8/coh/EP8_', freq, '_subj_plv_ConnectivityMatrix.txt'));
+R4 = dlmread(strcat('/home/custine/MEG/data/epi_conn/EP10/coh/EP10_', freq, '_subj_plv_ConnectivityMatrix.txt'));
 R = cat(3, R1, R2, R3, R4); 
 % whos
-label_names = csvread(strcat('/home/custine/MEG/results/source_level/ConnectivityPlots/label_names.csv'))
+%label_names = csvread(strcat('/home/custine/MEG/results/source_level/ConnectivityPlots/label_names.csv'))
+
 for i = 1:68
     x = squeeze(L(i,:,:));
     MX = mean(x,2);
@@ -43,7 +44,7 @@ for i = 1:68
 end
 
 
-dlmwrite(strcat('/home/custine/MEG/results/source_level/ConnectivityPlots/Hotel-Tsq_GrandAvgConnectivityMatrix_', freq , '_Left-Right.txt'), tsq_temp', 'delimiter', ' ')
+dlmwrite(strcat('/home/custine/MEG/results/source_level/ConnectivityPlots/Hotel-Tsq_PLV_GrandAvgConnectivityMatrix_', freq , '_Left-Right.txt'), tsq_temp', 'delimiter', ' ')
 
 end
 
