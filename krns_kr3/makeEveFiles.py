3368f6eaa2bcee63b268ec8d7de42399faa6f17b
CandidaUstine <cu38@cornell.edu>
2014-12-11 15:56:32 -0600

    updated commands

--- krns_kr3/makeEveFiles.py
+++ krns_kr3/makeEveFiles.py
@@ -1,28 +1,16 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Sep 12 11:53:19 2014
 
 @author: custine
+>>python makeEveFiles.py 9367 s8 
 """
-
-#import mne
-#from mne import io
-#
-#raw = mne.io.Raw('/home/custine/MEG/data/krns_kr3/9367/s5/9367_s5_run1_raw.fif')
-#eve_fname = '/home/custine/MEG/data/krns_kr3/9367/s5/eve/9367_s5_run1.eve'
-#print(raw)
-#print raw.info['sfreq']
-#events = mne.find_events(raw, stim_channel = 'STI102', min_duration = 0.001)
-#print(events)
-#
-#mne.write_events(eve_fname, events)
-
 
 print(__doc__)
 
 import mne
 from mne import io
 import argparse
+from mne import fiff
 
 parser = argparse.ArgumentParser(description='Get input')
 parser.add_argument('subj',type=str)
@@ -36,6 +24,8 @@ print sessID
 ###Event file suffix 
 eve_file = '.eve'
 runs = ['1', '2', '3', '4', '5','6', '7', '8', '9', '10', '11', '12']  # TESTING################################################
+#runs = ['9']  # TESTING################################################
+
 print runs
 
 for runID in runs:  ###For other runs use the baseline and set tmin/tmax using epoched data. 
@@ -47,6 +37,8 @@ for runID in runs:  ###For other runs use the baseline and set tmin/tmax using e
         print fname
         event_file = data_path + '/eve/' + subjID + '_'+ sessID +'_'+runID + eve_file
         raw = mne.io.Raw(fname)
+        print 'Reading Raw data... '
+        #raw = fiff.Raw(fname, preload = True)
         print(raw)
         print raw.info['sfreq']
         events = mne.find_events(raw, stim_channel = 'STI102', min_duration = 0.001)
