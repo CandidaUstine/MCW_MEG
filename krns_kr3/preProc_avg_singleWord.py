1c6bc9233aeef579225436244cad8fcc79f52859
candidaustine <candida.ustine@gmail.com>
2015-05-06 18:17:38 -0500

    reading epochs for a single word in runs and saving it as a mat file

--- krns_kr3/preProc_avg_singleWord.py
+++ krns_kr3/preProc_avg_singleWord.py
@@ -21,7 +21,7 @@ def mnepy_avg_allw(subjID): #, sessID, eve, ssp_type):
     import copy
     import numpy as np
     from scipy import io
-    import readTable
+    from readTable import readTable 
     
     #######Get Input ##
     print subjID
@@ -40,25 +40,13 @@ def mnepy_avg_allw(subjID): #, sessID, eve, ssp_type):
     hp_cutoff = 0.7
     lp_cutoff = 50 
     #######Experiment specific parameters 
-    ###EventLabels and Runs
-#    runs = cc.runDict[eve] ##TESTING################################################
-#    print runs
-    runs = ['run', 'run2', 'run3', 'run4', 'run5', 'run6', 'run7', 'run8', 'run9', 'run10', 'run11', 'run12'] #'run1', 'run2'] #
+    ###SessIDs and Runs
+    runs = ['run1', 'run2', 'run3', 'run4', 'run5', 'run6', 'run7', 'run8', 'run9', 'run10', 'run11', 'run12'] #'run1', 'run2'] #
     sess = ['s5', 's6', 's7', 's8']
-    labelList = cc.condLabels[eve]
-    event_id = {}
-    condName = {}
-    for row in labelList:
-        event_id[row[1]] = int(row[0])
-        condName[row[1]] = row[1]
-    print event_id 
-    ###TimeWindow
+    ###Word TimeWindow
     tmin = -.1
-    tmax = float(cc.epMax[eve])
-    tmax = 0.6
-#    tmax = 3.00 #4Words Category 
+    tmax = 0.6 
     ########Artifact rejection parameters
-    ###General
     gradRej = 4000e-13
     magRej = 4000e-12
     magFlat = 1e-14
@@ -68,51 +56,57 @@ def mnepy_avg_allw(subjID): #, sessID, eve, ssp_type):
     evoked=[]
     evokedRuns =[]
     epochsRuns =[]
+    (ID, word, role) = readTable('/mnt/file1/binder/KRNS/info/krns_word_list.txt')
+#    print ID, word
+#    ID = ID[0:1]
+#    word = word[0:1]
 
-    
-    
-    for sessID in sess:
-        raw_data_path = '/home/custine/MEG/data/krns_kr3/' +subjID+'/'+sessID
-        data_path = '/home/custine/MEG/data/krns_kr3/' +subjID+'/'+sessID
-        for runID in runs:
-                print runID
-                ##Setup Subject Speciifc Information
-                event_file = data_path + '/eve/triggers/' + subjID + '_'+ sessID +'_'+runID +'_' + eve_file
-                print event_file
-                test_file = data_path + '/eve/triggers/' + subjID + '_'+ sessID +'_'+runID + '-epochs.mat'
-                
-                raw_file = raw_data_path + '/' + subjID + '_' + sessID+ '_' + runID + runSuffix ##Change this suffix if you are using SSP ##_clean_ecg_
-                avgLog_file = data_path + '/ave_projon/logs/' +subjID + '_' + sessID+ '_'+runID + '_'+eve+'_' + ssp_type +'-ave.log'
-                print raw_file, avgLog_file
-            
-                ##Setup Reading fiff data structure
-                print 'Reading Raw data... '
-                raw = fiff.Raw(raw_file, preload = True)
-                events = mne.read_events(event_file)
-#                print events
-#            
-#            mne.set_log_file(fname = avgLog_file, overwrite = True)
-#            
-#            ##Filter raw data 
-#            fiff.Raw.filter(raw, l_freq = hp_cutoff, h_freq = lp_cutoff)
-#            
-                ##Pick all channels 
-                picks = []
-                for i in range(raw.info['nchan']):
-                    picks.append(i)
-#
-                ## #Read Epochs and compute Evoked :) 
-                event_id = 45
-                print 'Reading Epochs from evoked raw file...'
-                epochs = mne.Epochs(raw, events, event_id, tmin, tmax, baseline = (-0.1,0),preload = True,reject=dict(mag=magRej, grad=gradRej))
-                print epochs
-                epochsRuns = copy.deepcopy(epochs)
-                epochs_data = epochs.get_data()
-                
-                print epochs_data.shape
-                print test_file
-                io.savemat(test_file, dict(epochs_data = epochs_data), oned_as = 'row')
+    for evid,w in zip(ID, word):
+        print evid, w
+        for sessID in sess:
+            raw_data_path = '/home/custine/MEG/data/krns_kr3/' +subjID+'/'+sessID
+            data_path = '/home/custine/MEG/data/krns_kr3/' +subjID+'/'+sessID
+            for runID in runs:
+                    print runID                 
                     
+                    ##Setup Subject Speciifc Information
+                    event_file = data_path + '/eve/triggers/' + subjID + '_'+ sessID +'_'+runID +'_' + eve_file
+                    print event_file
+                    epochs_file = data_path + '/epochs/' + runID + '/'+ subjID + '_'+ sessID +'_'+runID + '_' + evid.zfill(3) + str(w)+ '-epochs.mat'
+                    print epochs_file
+                    raw_file = raw_data_path + '/' + subjID + '_' + sessID+ '_' + runID + runSuffix ##Change this suffix if you are using SSP ##_clean_ecg_
+                    avgLog_file = data_path + '/ave_projon/logs/' +subjID + '_' + sessID+ '_'+runID + '_'+eve+'-ave.log'
+                
+                    ##Setup Reading fiff data structure
+                    print 'Reading Raw data... '
+                    raw = fiff.Raw(raw_file, preload = True)
+                    events = mne.read_events(event_file)
+                    
+        #            mne.set_log_file(fname = avgLog_file, overwrite = True)
+        #            ##Filter raw data 
+        #            fiff.Raw.filter(raw, l_freq = hp_cutoff, h_freq = lp_cutoff)
+                    
+                    ##Check if the event exists in the run else save empty mat file. 
+                    (x, y, run_eveID) = readTable(event_file)
+                    if evid in run_eveID:
+                        print "EventID exists in the run... "                        
+                        ##Pick all channels 
+                        picks = []
+                        for i in range(raw.info['nchan']):
+                            picks.append(i)
+                        ##Create epochs.mat file for specified event ID 
+                        print 'Reading Epochs from evoked raw file...'
+                        epochs = mne.Epochs(raw, events, int(evid), tmin, tmax, baseline = (-0.1,0),name = str(w), preload = True,reject=dict(mag=magRej, grad=gradRej))
+                        epochs_data = epochs.get_data()
+                        ##Saving Epochs in mat file 
+                        print epochs_data.shape
+                        print epochs_file
+                        io.savemat(epochs_file, dict(epochs_data = epochs_data), oned_as = 'row')
+                    else:
+                        epochs_empty = dict()
+                        print "EventID Does not exist in the run. Saving empty mat file... "
+                        io.savemat(epochs_file, epochs_empty, oned_as = 'row')
+                            
 
 if __name__ == "__main__":
     ####### Get Input ########
